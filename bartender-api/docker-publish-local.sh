d6e9e8498a08721d10f204a57736a65efa6ce960
Sam Allen <Sam.Allen@beveragebot.com>
2019-12-21 02:06:35 -0800

    Adding microfront ends and dockerfile

--- bartender-api/docker-publish-local.sh
+++ bartender-api/docker-publish-local.sh
@@ -1,4 +1,4 @@
 #!/usr/bin/env bash
-mvn clean install -DskipTest -Dspring.cloud.contract.verifier.skip=true -DskipTests
-docker build -t localhost:32000/bartender-api .
-docker push localhost:32000/bartender-api
+mvn clean install -Dspring.cloud.contract.verifier.skip=true -DskipTests
+docker build -t localhost:32000/browse-drink-api-gateway .
+docker push localhost:32000/browse-drink-api-gateway
